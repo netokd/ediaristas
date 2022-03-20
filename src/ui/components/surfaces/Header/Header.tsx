7fc63a1c5467db42cb60261b527fb02248f33e58
João Pedro <jponeto29@gmail.com>
2022-03-20 17:52:25 -0300

    Finalização do Header com responsividade pra mobile e desktop proporcional ao tamanho da tela com um menu drawer pra mobile

--- src/ui/components/surfaces/Header/Header.tsx
+++ src/ui/components/surfaces/Header/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { HeaderAppBar, HeaderLogo, ButtonsContainer } from "../Header/Header.style";
-import { Container, Toolbar, IconButton } from "@material-ui/core";
+import { HeaderAppBar, HeaderLogo, ButtonsContainer, HeaderDrawer } from "../Header/Header.style";
+import { Container, Toolbar, IconButton, MenuList, MenuItem, Divider } from "@material-ui/core";
 import Link from "ui/components/navigation/Link/Link";
 import RoundedButton from "ui/components/inputs/RoundedButton/RoundedButton";
+import useIsMobile from "data/hooks/useIsMobile";
+import {useState} from 'react';
 
 
 const Header = () => {
-    return <HeaderMobile/>;
+    const isMobile = useIsMobile();
+    return isMobile ? <HeaderMobile/> : <HeaderDesktop/>
 };
 
 const HeaderDesktop = () => {
@@ -39,12 +42,36 @@ const HeaderDesktop = () => {
 };
 
 const HeaderMobile = () => {
+    const [isDrawerOpen, setDrawerOpen] = useState(false);
     return(
         <HeaderAppBar>
             <Toolbar component={Container} >
-                <IconButton>
+                <IconButton edge={'start'} color={'inherit'} onClick={()=> setDrawerOpen(true)}>
                     <i className={'twf-bars'} />
                 </IconButton>
+
+                <Link href={'/'}>
+                    <HeaderLogo
+                        src={'/img/logos/logo.svg'}
+                        alt={'e-diaristas'}
+                    />
+                </Link>
+
+                <HeaderDrawer 
+                    open={isDrawerOpen} 
+                    onClose={()=> setDrawerOpen(false)}
+                    onClick={()=> setDrawerOpen(false)}
+                >
+                    <MenuList>
+                        <Link href={'/login'} Component={MenuItem} >Login</Link>
+
+                        <Divider/>
+
+                        <Link href={'/cadastro/diarista'} Component={MenuItem} >
+                            Seja um(a) diarista
+                        </Link>
+                    </MenuList>
+                </HeaderDrawer>
             </Toolbar>
         </HeaderAppBar>
     )
